41ef377fdd24c7c362be99dc5506c22926442c00
Bryyyyy <bryancalulo@gmail.com>
2022-11-12 21:07:26 +0800

    c

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Hello world!" << endl;
     cout << " Ha ha ha\n" ;
-    cout << "Sekyuriti Sekyuriti\n" ;
+    cout << "Sekyurggiti Sekyuriti\n" ;
     cout << "Inbentoryo \n" ;
 
     cout << "ignore\n" ;
